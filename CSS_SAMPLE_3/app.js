17858b82d9609df906b4d89488c355003db339d7
Mohd Ahshan Danish <mailtodanish@gmail.com>
2022-06-16 10:56:15 +0530

    Update app.js

--- CSS_SAMPLE_3/app.js
+++ CSS_SAMPLE_3/app.js
@@ -1,24 +1,8 @@
-function animate(el, open, delay) {
-    console.error(delay);
-    el.style.transitionDelay = `${delay}ms`;
-    if (open) {
-        el.classList.remove('folderPanelClosed');
-    } else {
-        el.classList.add('folderPanelClosed');
-    }
-}
-// (function() {
-//     let socials = document.querySelectorAll(".container .card");
-//     socials.forEach(function(social, index) {
-//         animate(social, true, index * 2 * 317);
-
-//     });
-// })();
-
 function animation() {
-    let socials = document.querySelectorAll(".container .card");
-    socials.forEach(function(social, index) {
-        animate(social, true, index * 2 * 317);
+    let els = document.querySelectorAll(".container .card");
+    els.forEach(function(el, index) {
+        el.style.transitionDelay = `${index * 2 * 317}ms`;
+        el.classList.remove('folderPanelClosed');
 
     });
 };
